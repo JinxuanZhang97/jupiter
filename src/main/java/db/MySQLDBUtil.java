15c977f232d6f25517ab25a5afbb4769341674be
Patricia_Z <jinxuanz@usc.edu>
2020-09-23 17:23:10 -0700

    My first commit

--- src/main/java/db/MySQLDBUtil.java
+++ src/main/java/db/MySQLDBUtil.java
@@ -5,7 +5,7 @@ public class MySQLDBUtil {
 	private static final String PORT_NUM = "3306";
 	public static final String DB_NAME = "jupiter";
 	private static final String USERNAME = "admin";
-	private static final String PASSWORD = "bbysx66b";
+	private static final String PASSWORD = "";
 	public static final String URL = "jdbc:mysql://"
 			+ INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
 			+ "?user=" + USERNAME + "&password=" + PASSWORD
